f25aa8bd8d5c91de70ce690fa6818ab9b6127143
sauzansalsabila <118974171+sauzansalsabila@users.noreply.github.com>
2023-01-06 09:51:22 +0700

    Update settings.py

--- utspbo/settings.py
+++ utspbo/settings.py
@@ -25,9 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%xw!wj1qrflb-z0wz1i+_2806@1^@racc7f7l!k2ux+jkal*xd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
+
+ALLOWED_HOSTS = ["https://web-production-3468.up.railway.app"]
 
 
 # Application definition
