ddce9aca3217b9e844f0280d57961f6ab8670be5
sauzansalsabila <118974171+sauzansalsabila@users.noreply.github.com>
2023-01-06 13:18:56 +0700

    jadi dong

--- utspbo/settings.py
+++ utspbo/settings.py
@@ -17,8 +17,6 @@ import sqlite3
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CSRF_TRUSTED_ORIGINS 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
